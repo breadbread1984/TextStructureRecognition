3ec5602cdc0b1d03ec7cbce90ac71ddfd350a388
解易 <xieyi@aksofy.cn>
2020-06-27 15:37:09 +0800

    update model

--- models.py
+++ models.py
@@ -25,6 +25,7 @@ def GraphAdjacentLayer(d_in, num_dims, jump = 1, use_dropout = False, activation
   results = tf.keras.layers.BatchNormalization()(results);
   results = tf.keras.layers.LeakyReLU()(results);
   results = tf.keras.layers.Dense(units = jump)(results); # results.shape = (batch, N, N, jump)
+  # filter adjacent nodes which are already reachable in less than j jumps
   if activation == 'softmax':
     results = tf.keras.layers.Lambda(lambda x: x[0] - x[1] * 1e8)([results, w]); # results.shape = (batch, N, N, jump)
     flatten = tf.keras.layers.Reshape((-1, jump))(results); # flatten.shape = (batch, N * N, jump)
@@ -59,19 +60,26 @@ def GConv(d_in, d_out, jump = 1):
   results = tf.keras.layers.BatchNormalization()(results); # results.shape = (batch, N, d_out)
   return tf.keras.Model(inputs = (x, w), outputs = results);
 
-def GNN(d_in, num_dims, num_layers, num_classes):
+def GNN(d_in, num_dims, num_layers, num_classes, has_initial_weight = False):
 
   x = tf.keras.Input((None, d_in)); # x.shape = (batch, N, d_in)
-  w = tf.keras.layers.Lambda(lambda x: tf.tile(tf.reshape(tf.eye(tf.shape(x)[1]), (1,tf.shape(x)[1],tf.shape(x)[1],1)), (tf.shape(x)[0], 1, 1, 1)))(x); # w.shape = (batch, N, N, 1)
+  if has_initial_weight:
+    w = tf.keras.Input((None, None, 1)); # w.shape = (batch, N, N, 1);
+  else:
+    w = tf.keras.layers.Lambda(lambda x: tf.tile(tf.reshape(tf.eye(tf.shape(x)[1]), (1,tf.shape(x)[1],tf.shape(x)[1],1)), (tf.shape(x)[0], 1, 1, 1)))(x); # w.shape = (batch, N, N, 1)
   prev_x = x;
+  prev_w = w;
   for i in range(num_layers):
-    w = GraphAdjacentLayer(d_in = d_in + (num_dims // 2) * i, num_dims = num_dims, jump = 2**i, operator = 'J2')([prev_x, w]); # w.shape = (batch, N, N, 2)
-    cur_x = GConv(d_in = d_in + (num_dims // 2) * i, d_out = num_dims // 2, jump = 2**(i+1))([prev_x, w]); # x_new.shape = (batch, N, num_dims // 2)
+    prev_w = GraphAdjacentLayer(d_in = d_in + (num_dims // 2) * i, num_dims = num_dims, jump = 2**i, operator = 'J2')([prev_x, prev_w]); # prev_w.shape = (batch, N, N, 2)
+    cur_x = GConv(d_in = d_in + (num_dims // 2) * i, d_out = num_dims // 2, jump = 2**(i+1))([prev_x, prev_w]); # x_new.shape = (batch, N, num_dims // 2)
     cur_x = tf.keras.layers.LeakyReLU()(cur_x); # x_new.shape = (batch, N, num_dims // 2)
     prev_x = tf.keras.layers.Concatenate()([prev_x, cur_x]); # x.shape = (batch, N, num_dims * 1.5)
-  w = GraphAdjacentLayer(d_in = d_in + (num_dims // 2) * num_layers, num_dims = num_dims, jump = 2**num_layers, operator = 'J2')([prev_x, w]); # w.shape = (batch, N, N, 2)
-  results = GConv(d_in = d_in + (num_dims // 2) * num_layers, d_out = num_classes, jump = 2**(num_layers + 1))([prev_x, w]); # x.shape = (batch, N, num_classes)
-  return tf.keras.Model(inputs = x, outputs = results);
+  prev_w = GraphAdjacentLayer(d_in = d_in + (num_dims // 2) * num_layers, num_dims = num_dims, jump = 2**num_layers, operator = 'J2')([prev_x, prev_w]); # prev_w.shape = (batch, N, N, 2)
+  results = GConv(d_in = d_in + (num_dims // 2) * num_layers, d_out = num_classes, jump = 2**(num_layers + 1))([prev_x, prev_w]); # x.shape = (batch, N, num_classes)
+  if has_initial_weight:
+    return tf.keras.Model(inputs = (x,w), outputs = results);
+  else:
+    return tf.keras.Model(inputs = x, outputs = results);
 
 if __name__ == "__main__":
 
@@ -86,7 +94,8 @@ if __name__ == "__main__":
   gc.save('gc.h5');
   b = gc([a,w]);
   print(b.shape);
-  gnn = GNN(128, 128, 3, 10);
+  gnn = GNN(128, 128, 3, 10, True);
   gnn.save('gnn.h5');
+  w = tf.constant(np.random.normal(size = (8, 10, 10, 1)), dtype = tf.float32);
   b = gnn([a,w]);
   print(b.shape);
