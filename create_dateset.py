46236562c795ed9612c83c4ae4db411ceda2ef24
解易 <xieyi@aksofy.cn>
2020-06-27 19:21:47 +0800

    update create dataset

--- create_dateset.py
+++ create_dateset.py
@@ -1,40 +1,56 @@
 #!/usr/bin/python3
 
-from os.path import join;
+import sys;
+from os.path import join, exists;
 import json;
+import numpy as np;
 import cv2;
 import tensorflow as tf;
 
-def load_boundings(anno_path, imdir = '.'):
+def create_dataset(dataset_path):
 
-  with open(anno_path, 'r') as f:
+  if not exists(dataset_path): return False;
+  if not exists('datasets'): mkdir('datasets');
+  writer = tf.io.TFRecordWriter(join('datasets', 'trainset.tfrecord'));
+  with open(join(dataset_path, 'via_project.json'), 'r') as f:
     via = json.load(f);
-  retval = list();
   for fid in via['_via_img_metadata']:
-    fn = join(imdir, via['_via_img_metadata'][fid]['filename']);
+    fn = join(dataset_path, 'images', via['_via_img_metadata'][fid]['filename']);
     img = cv2.imread(fn);
     if img is None:
       print('cannot open image %s' % (fn));
       continue;
+    width = img.shape[1];
+    height = img.shape[0];
+    positions = list();
     for region in via['_via_img_metadata'][fid]['regions']:
       if region['shape_attributes']['name'] != 'rect':
         print('extraction of %s regions not yet implemented!' % region['shape_attributes']['name']);
         continue;
-      x = region['shape_attributes']['x']
-      y = region['shape_attributes']['y']
-      w = region['shape_attributes']['width']
-      h = region['shape_attributes']['height']
+      x = region['shape_attributes']['x'];
+      y = region['shape_attributes']['y'];
+      w = region['shape_attributes']['width'];
+      h = region['shape_attributes']['height'];
       left = int(max(0, x));
       top = int(max(0, y));
       right = int(min(img.shape[1], x + w));
-      bottom = int(min(img.shape[0], y + h));
+      bottom = int(min(img.shape[0], y + h));      
       crop = img[top:bottom, left:right, :];
-      retval.append({'img': crop, 'pos': (left, top, right, bottom)});
-  return retval;
+      positions.append([left / width, top / height, right / width, bottom / height]);
+    embeddings = tf.constant(np.array(positions, dtype = np.float32)); # embeddings.shape = (N, 4)
+    weights = tf.linalg.band_part(tf.ones((embeddings.shape[0],embeddings.shape[0])), 1, 1) - tf.eye(embeddings.shape[0]); # weights.shape = (N, N)
+    trainsample = tf.train.Example(features = tf.train.Features(
+      feature = {
+        'embedings': tf.train.Feature(float_list = tf.train.FloatList(value = embeddings.reshape(-1))),
+        'weights': tf.train.Feature(float_list = tf.train.FloatList(value = weights.reshape(-1)))}));
+    writer.write(trainsample.SerializeToString());
+  writer.close();
+  return True;
 
 if __name__ == "__main__":
 
-  bounds = load_boundings('/mnt/c/Users/Lenovo/Downloads/reading order example/before.json', '/mnt/c/Users/Lenovo/Downloads/reading order example/');
-  for bound in bounds:
-    cv2.imshow('img', bound['img']);
-    cv2.waitKey();
+  assert tf.executing_eagerly();
+  if len(sys.argv) != 2:
+    print("Usage: " + sys.argv[0] + " <dataset dir>");
+    exit();
+  create_dataset(sys.argv[1]);
