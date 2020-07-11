fdd5cbf370b97f405d3aba0457215653faab5a00
解易 <xieyi@aksofy.cn>
2020-07-11 15:48:23 +0800

    update create dataset

--- create_dataset.py
+++ create_dataset.py
@@ -76,7 +76,7 @@ def create_dataset(dataset_path):
       feature = {
         'num': tf.train.Feature(int64_list = tf.train.Int64List(value = [len(features)])),
         'embeddings': tf.train.Feature(float_list = tf.train.FloatList(value = embeddings.reshape(-1))),
-        'labels': tf.train.Feature(int64_list = tf.train.Int64List(value = labels.reshape(-1))),
+        'labels': tf.train.Feature(int64_list = tf.train.Int64List(value = labels)),
         'weights': tf.train.Feature(float_list = tf.train.FloatList(value = weights.numpy().reshape(-1)))}));
     writer.write(trainsample.SerializeToString());
     data_num += 1;
